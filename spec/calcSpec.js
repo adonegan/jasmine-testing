15b68f48bf21001508d628b733499523804de7a4
Ubuntu <ubuntu@ip-172-31-27-100.ec2.internal>
2019-06-20 15:10:21 +0000

    refactoring: setting up each spec

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,8 +1,9 @@
 describe("Calculator", function() {
     
-    var calc = new Calculator();
+    beforeEach(function() {
+        calc = new Calculator();
+    })
     
-
     describe("Addition tests", function() {
         it("should return 42", function() {
             calc.add(20);
